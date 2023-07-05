51f30cb5a3cef4ba6771285ea53cff9de43ab59a
luis12007 <00129020@edu.sv>
2023-07-05 11:33:10 -0600

    ps

--- src/main/java/com/ticketya/ticketsweb/TicketYaApiApplication.java
+++ src/main/java/com/ticketya/ticketsweb/TicketYaApiApplication.java
@@ -1,10 +1,12 @@
 package com.ticketya.ticketsweb;
 
-
+import java.security.SecureRandom;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class TicketYaApiApplication {
@@ -13,5 +15,9 @@ public class TicketYaApiApplication {
 		SpringApplication.run(TicketYaApiApplication.class, args);
 	}
 	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder(5, new SecureRandom());
+	}
 
 }
